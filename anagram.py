439239aca12e24c740352774a8b4a9d1cde535b5
Adelice <nancytoniw@gmail.com>
2021-06-03 14:46:03 +0200

    first commit

--- anagram.py
+++ anagram.py
@@ -1,8 +1,13 @@
 def anagram(p,s):
     a=3
     p=["abc","bca","cab","acb","bac"]
-    #Split the string to the 3rd character
+    #Split the string to the 3rd character and have list of substring
     res=[s[y-a:y] for y in range(a, len(s)+a,a)]
+    if p in res:
+        print(res.find(p))
+   
+        
+
      
   
       
